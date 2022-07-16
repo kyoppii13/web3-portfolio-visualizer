cfaf43e1187ed9c5b7b724a0b49922643ea2d745
kyoppii13 <kyou1013@gmail.com>
2022-07-16 19:20:29 +0900

    add chart

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,48 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Stack } from "@chakra-ui/react";
 import { FC, memo } from "react";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export const Home: FC = memo(() => {
   return (
     <Flex align="center" justify="center" height="100vh">
-      <Heading>home</Heading>
+      <Box w="sm">
+        <Stack>
+          <Heading textAlign="center">home</Heading>
+          <Line
+            height="100%"
+            width="100%"
+            data={{
+              labels: ["Jun", "Jul", "Aug"],
+              datasets: [
+                {
+                  label: "1",
+                  data: [5, 6, 7],
+                },
+              ],
+            }}
+          />
+        </Stack>
+      </Box>
     </Flex>
   );
 });
